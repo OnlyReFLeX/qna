8c5cb0dab0e0521b03de258229101f892f60db72
OnlyReFLeX <reflexonly@gmail.com>
2018-04-13 17:20:06 +0300

    refactoring

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -18,18 +18,18 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'POST #create' do
     context 'Passing with valid parameters' do
-      it 'The answer is stored in the database and is attached to the question' do
+      it 'stores answer in DB' do
         expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(question.answers, :count).by(1)
       end
 
-      it 'Redirect to show question after create' do
+      it 'Redirect to show question' do
         post :create, params: { question_id: question, answer: attributes_for(:answer) }
         expect(response).to redirect_to question_path(question)
       end
     end
 
     context 'Passing with non-valid parameters' do
-      it 'The fact that the answer is not saved' do
+      it 'answer is not saved' do
         expect { post :create, params: { question_id: question, answer: attributes_for(:invalid_answer)} }.to_not change(Answer, :count)
       end
 
