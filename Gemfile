6a1370e3d32cd3e908c1012b86dd0713401d2853
OnlyReFLeX <reflexonly@gmail.com>
2018-04-18 22:24:06 +0300

    devise install

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ group :test do
 end
 
 gem 'slim-rails', '~> 3.1'
+
+gem 'devise', '~> 4.4'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'questions#index'
+
   resources :questions, only: [:new, :show, :create, :index] do
     resources :answers, only: [:create], shallow: true
   end
