d139a1e7e75b847a326f696dbc24ee7ad0edf365
OnlyReFLeX <reflexonly@gmail.com>
2018-04-20 20:08:45 +0300

    fix

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should belong_to(:user) }
   it { should have_many(:answers).dependent(:destroy) }
-  it { should have_db_column(:user_id) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
 end
