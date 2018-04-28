9a41e12cf14768a80845bebb4cf0fd73b79846ca
OnlyReFLeX <reflexonly@gmail.com>
2018-04-28 19:39:13 +0300

    Implement the ability to add multiple files when creating a question or answer

--- spec/features/answers/add_files_to_answer_spec.rb
+++ spec/features/answers/add_files_to_answer_spec.rb
@@ -6,7 +6,7 @@ feature 'Add files to answer', %q{
   I'd like to be able to attach files
 } do
 
-  given(:user) { create(:user) }
+  given!(:user) { create(:user) }
   given(:question) { create(:question, user: user) }
 
   background do
@@ -15,14 +15,37 @@ feature 'Add files to answer', %q{
   end
 
   scenario 'User adds file to answer', js: true do
-    within '.new_answer_form' do
-      fill_in 'Body', with: 'My answer'
-      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-      click_on 'Create'
-    end
+    fill_in 'Body', with: 'Answer text'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'Create Answer'
 
     within '.answers' do
-      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
     end
+  end
+
+  scenario 'The user adds more than one file', js: true do
+    fill_in 'Body', with: 'My answer'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_link 'Add attachment'
+    within first(".nested-fields") do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+    click_on 'Create'
+
+    within '.answers' do
+      expect(page).to have_link 'spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb'
+    end
+  end
+
+  scenario 'User deletes the file when creating', js: true do
+    within '.new_answer_form' do
+      fill_in 'Body', with: 'My answer'
+    end
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_link 'Delete'
+    click_on 'Create'
+    expect(page).to_not have_link 'spec_helper.rb'
   end
 end
--- spec/features/questions/add_files_to_question_spec.rb
+++ spec/features/questions/add_files_to_question_spec.rb
@@ -11,14 +11,31 @@ feature 'Add files to question', %q{
   background do
     sign_in(user)
     visit new_question_path
-  end
-
-  scenario 'User adds file when asks question' do
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'text text text'
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+  end
+
+  scenario 'User adds file when asks question' do
     click_on 'Create'
 
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+  end
+
+  scenario 'The user adds more than one file', js: true do
+    click_link 'Add attachment'
+    within first(".nested-fields") do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+    click_on 'Create'
+
+    expect(page).to have_link 'spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb'
+  end
+
+  scenario 'User deletes the file when creating', js: true do
+    click_link 'Delete'
+    click_on 'Create'
+    expect(page).to_not have_link 'spec_helper.rb'
   end
 end
