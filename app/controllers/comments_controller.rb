d50bebca85b65bb31bc5b9b2f78d4ed11c240734
OnlyReFLeX <reflexonly@gmail.com>
2018-05-05 14:27:12 +0300

    refactoring

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,25 +4,13 @@ class CommentsController < ActionController::Base
   def create
     @comment = @resource.comments.build(comment_params)
     @comment.user = current_user
-    if @comment.save
-      render_json_details
-    else
-      render_json_errors
-    end
+    @comment.save
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:body, :resource_id)
-  end
-
-  def render_json_details
-    render json: { comment: @comment.body, klass: @comment.commentable.class.to_s, id: @comment.commentable.id }
-  end
-
-  def render_json_errors
-    render json: { error: @comment.errors.full_messages.join("\n"), klass: @comment.commentable.class.to_s, id: @comment.commentable.id  }, status: :unprocessable_entity
   end
 
   def find_resource
