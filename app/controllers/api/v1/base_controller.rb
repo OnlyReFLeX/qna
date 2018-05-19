4788bac68bf44c799f377352b1087c8d05198bd8
OnlyReFLeX <reflexonly@gmail.com>
2018-05-19 22:12:55 +0300

    refactoring

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -8,7 +8,5 @@ class Api::V1::BaseController < ApplicationController
     @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
 
-  def current_user
-    current_resource_owner
-  end
+  alias :current_user :current_resource_owner
 end
