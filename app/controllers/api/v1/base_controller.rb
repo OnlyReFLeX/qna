3200e6155853d5e68a43b910a8c07145cd5394bb
OnlyReFLeX <reflexonly@gmail.com>
2018-05-20 14:23:35 +0300

    refactoring
    Since alias, like def, is a keyword, prefer bareword arguments over symbols or strings. In other words, do alias foo bar, not alias :foo :bar.

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -8,5 +8,5 @@ class Api::V1::BaseController < ApplicationController
     @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
 
-  alias :current_user :current_resource_owner
+  alias current_user current_resource_owner
 end
